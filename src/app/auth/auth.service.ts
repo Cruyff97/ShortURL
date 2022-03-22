e1eea03343bf377b23a9107815d43c0f4916e564
cruyff97 <pelmat97@gmail.com>
2022-03-22 22:52:38 +0100

    try proxy

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -46,7 +46,7 @@ export class AuthService {
       'Content-Type': 'application/json',
       'Authorization': `${token}`
     });
-    return this.http.get<IMyUrls>('/api/user/urls', {headers: headers} )
+    return this.http.get<IMyUrls>('https://croppy.herokuapp.com/api/user/urls', {headers: headers} )
   }
   constructor(private http: HttpClient) {}
 }
