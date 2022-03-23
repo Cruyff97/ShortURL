fbb82839faafefd33a7abeff510c98c2ef811e1f
cruyff97 <pelmat97@gmail.com>
2022-03-23 15:22:46 +0100

    feat: 🎸 add hamburger menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,15 @@
 import { Router } from '@angular/router';
 import { AuthService } from './../auth/auth.service';
 import { Component, OnInit } from '@angular/core';
-
+import { trigger, state, style, animate, transition, query, stagger } from '@angular/animations';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-signedin=false
+collapse=false;
+  signedin=false
 token=localStorage.getItem('id_token');
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -18,12 +19,20 @@ token=localStorage.getItem('id_token');
    }
    else this.signedin=false
   }
+  menuOnClick() {
+    document.getElementById("menu-bar")?.classList.toggle("change");
+    document.getElementById("nav")?.classList.toggle("change");
+    document.getElementById("menu-bg")?.classList.toggle("change-bg");
+  }
 onSignOut(){
   this.authService.logout();
   this.authService.signedin$.subscribe((signedin: any) => {
     this.signedin= signedin;
  
   })
+  this.menuOnClick();
   this.router.navigate(['/home']);
 }
+
+
 }
